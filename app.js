b95334fb5772afbc1922b49134cef4416440bfa8
webagent-dev <webagent24@gmail.com>
2023-11-22 21:25:37 +0100

    toggle setup complete

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 
-// function to toggle model
+// function to remove model
 
 function removeModel(){
     const closeModel = document.querySelector('.model_container');
-        closeModel.remove()
+        closeModel.remove();
 }
+
+// function to toggle setup
+
+function toggleSetup(){
+    const setupToggle = document.getElementById('guide_container_wrapper');
+    const toggleImg = document.querySelector('.toggle');
+    // console.log(setupToggle);
+    setupToggle.classList.toggle('seen_list');
+   const checkClass = setupToggle.classList.contains('seen_list');
+    if(checkClass){
+        toggleImg.src='https://crushingit.tech/hackathon-assets/icon-arrow-up.svg'
+    }else{
+        toggleImg.src='https://crushingit.tech/hackathon-assets/icon-arrow-down.svg'
+    }
+  
+    }
+   
